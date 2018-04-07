4747f8a1e90c288dc1dcde2a18bb07c29359ac77
julianalonzo <julianaubreya@gmail.com>
2018-04-07 22:57:46 +0800

    Added scroll to content feature

--- website-v1.1/script/myScript.js
+++ website-v1.1/script/myScript.js
@@ -1,4 +1,6 @@
-// document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
+    window.dispatchEvent(new Event("scroll"));
+    
     // Progress bar
     if (document.getElementById("progressBar")) {
         document.addEventListener("scroll", function() {
@@ -14,14 +16,18 @@
 
     // Table of Contents
     var topicTitles = document.getElementsByClassName("topic-title");
-
+    var contentIndicators = document.getElementsByClassName("content-list");
     var lastScrollTop = 0;
-    window.addEventListener("scroll", function() {
-        var currentSectionIndex = 0;
-        var previousIndex;
+    var currentSectionIndex = 0;
+    var previousSectionIndex = -1;
+    var currentIndicator;
+    var previousIndicator;
+
+    function updateCurrentTopic() {
+        console.log("Scrolled...");
 
         for (var i = 0; i < topicTitles.length; i++) {
-            if (topicTitles[i].getBoundingClientRect().top < 100) {
+            if (topicTitles[i].getBoundingClientRect().top < 10) {
                 currentSectionIndex = i;
             } else if (progressBar.value == 100) {
                 currentSectionIndex = topicTitles.length - 1;
@@ -29,20 +35,29 @@
         }
 
         if (window.scrollY > lastScrollTop) {
-            previousIndex = currentSectionIndex - 1;
+            previousSectionIndex = currentSectionIndex - 1;
         } else if (window.scrollY < lastScrollTop && currentSectionIndex < topicTitles.length - 1) {
-            previousIndex = currentSectionIndex + 1;
+            previousSectionIndex = currentSectionIndex + 1;
         }
 
-        var currentIndicator = document.querySelector("[data-target=\"#" + topicTitles[currentSectionIndex].id + "\"]");
+        currentIndicator = document.querySelector(`[data-target=${topicTitles[currentSectionIndex].id}]`);
         currentIndicator.classList.add("current-section");
 
-        if (currentSectionIndex != previousIndex && previousIndex >= 0) {
-            var previousIndicator = document.querySelector("[data-target=\"#" + topicTitles[previousIndex].id + "\"]");
+        if (currentSectionIndex != previousSectionIndex && previousSectionIndex >= 0) {
+            previousIndicator = document.querySelector(`[data-target=${topicTitles[previousSectionIndex].id}]`);
             previousIndicator.classList.remove("current-section");
         }
 
         lastScrollTop = window.scrollY;
+    }
+
+    window.addEventListener("scroll", updateCurrentTopic);
+
+    // Table of Contents clicked
+    document.querySelector("#tableOfContents > ol").addEventListener("click", function(e) {
+        if (e.target && e.target.nodeName == "LI") {
+            window.location.href = `#${e.target.getAttribute("data-target")}`;
+        }
     });
 
     // Toggle sidebar
@@ -73,5 +88,4 @@
             }
         });
     }
-// });
-
+});
